7b1a87b7b1586e68ef782f967c1d2fd9d2c8df2e
TobiasKb96 <tobias.kaltenbrunner1@stud.fh-campuswien.ac.at>
2024-01-21 18:18:16 +0100

    made screen size the same and made screen size not adjustable

--- src/main/java/com/quizmaker/MainApp.java
+++ src/main/java/com/quizmaker/MainApp.java
@@ -2,6 +2,7 @@ package com.quizmaker;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -14,10 +15,11 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("menuScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 667,400);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
-
     }
 
     //old Console Output code
@@ -122,7 +124,6 @@ public class MainApp extends Application {
 
         //Old console Output code
         //gameLoop();
-
         launch();
 
     }
